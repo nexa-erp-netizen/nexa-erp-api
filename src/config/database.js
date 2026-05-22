e1a8e8a654d9d8db51b87f5f24a05a5de707e92e
unknown <assessoriacontabil.infinity@gmail.com>
2026-05-22 18:23:35 -0300

    Configuração banco online Supabase

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(
-  "nexa_erp",
-  "postgres",
-  "Nexa123@",
-  {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
-    logging: false,
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+})
 
 module.exports = sequelize
